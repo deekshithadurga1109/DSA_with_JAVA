ed25cf88d7ed5c160ff87980c0f2769cf8776d80
DEEKSHITHA KAMMULA <125685615+deekshithadurga1109@users.noreply.github.com>
2025-07-19 00:03:20 +0530

    Update DiamondPattern.java

--- DAY3/AdvPatterns/DiamondPattern.java
+++ DAY3/AdvPatterns/DiamondPattern.java
@@ -1,3 +1,36 @@
 public class DiamondPattern {
-    
+    public static void diamond(int n){
+        // ist half
+        for(int i=1; i<=n; i++){
+            //spaces 
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        // 2nd half 
+
+                // ist half
+        for(int i=n; i>=i; i--){
+            //spaces 
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        diamond(6);
+    }
 }
