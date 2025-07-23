805b20e079a34ace0c774e3a39b0fb7532154672
DEEKSHITHA KAMMULA <125685615+deekshithadurga1109@users.noreply.github.com>
2025-07-23 23:31:50 +0530

    Add files via upload

---
+++ DAY8/Bit Manipulation/BitwiseOperations.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class BitwiseOperations {
+    public static void main(String args[]){
+        System.out.println(5 & 6);
+        System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
+        System.out.println(~0); 
+        System.out.println(5 << 2); 
+        System.out.println(6 >> 1);   
+    }
+}
---
+++ DAY8/Bit Manipulation/UpdateBit.java
@@ -0,0 +1,36 @@
+public class UpdateBit {
+
+        public static int setBit(int n ,int i) {
+       int bitMask = 1<<i;
+       return n | bitMask;
+    }
+
+     public static int clearBit(int n , int i){
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+
+    }
+
+
+    public static int updateIthBit(int n, int i, int newBit) {
+    //  if(newBit == 0) {
+    //     return clearBit(n, i);
+    //  } else {
+    //     return setBit(n, i);
+    //  }
+
+    // 2nd way 
+
+   n = clearBit(n, i);
+  int BitMask =  newBit<<i;
+  return n | BitMask;
+
+
+    }
+    
+
+   public static void main(String args[]){
+    System.out.println(updateIthBit(10, 2, 1));
+   } 
+    
+}
