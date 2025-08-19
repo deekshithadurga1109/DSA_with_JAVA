44fff76635851cf9bd72f63b5ed72fbdafc21ba7
DEEKSHITHA KAMMULA <125685615+deekshithadurga1109@users.noreply.github.com>
2025-08-19 21:56:39 +0530

    Add files via upload

---
+++ DAY35/Approach2Diameter.java
@@ -0,0 +1,104 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Approach2Diameter {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    public static int height(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int ln = height(root.left);
+        int rh = height(root.right);
+        return Math.max(ln, rh)+ 1;
+    }
+
+    public static int count(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftCount = count(root.left);
+        int rightCount = count(root.right);
+        return leftCount + rightCount + 1;
+    }
+
+    public static int sum(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftSum = sum(root.left);
+        int rightSum = sum(root.right);
+        return leftSum + rightSum + root.data;
+    }
+  // Diameter function to find Diameter of the Treee 
+    public static int diameter2(Node root) {  //O(N^2)
+        if(root == null) {
+            return 0;
+        }
+        int leftDiam = diameter2(root.left);
+        int rightDiam = diameter2(root.right);
+        int leftHt = height(root.left);
+        int rightHt = height(root.right);
+
+        int selfDiam = leftHt + rightHt + 1;
+        return Math.max(selfDiam, Math.max(leftDiam,rightDiam));
+
+    }
+
+    static class Info {
+        int diam;
+        int ht;
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter(Node root) { // O(N)
+        if(root ==null) {
+            return new Info(0,0);
+        }
+        Info leInfo = diameter(root.left);
+        Info righInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leInfo.diam,righInfo.diam), leInfo.ht + righInfo.ht + 1);
+        int ht = Math.max(leInfo.ht, righInfo.ht) + 1;
+
+        return new Info(diam, ht);
+    }
+    public static void main(String[] args) {
+        /*
+               1
+              / \
+             2   3
+            / \ / \
+           4  5 6  7
+
+         */
+
+         Node root = new Node(1);
+         root.left = new Node(2);
+         root.right = new Node(3);
+         root.left.left = new Node(4);
+         root.left.right = new Node(5);
+         root.right.left = new Node(6);
+         root.right.right = new Node(7);
+
+        //  System.out.println(height(root));
+        // System.out.println(count(root));
+        // System.out.println(sum(root));
+
+        System.out.println(diameter(root).diam);
+    }
+    
+}
